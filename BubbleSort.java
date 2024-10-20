14a94d308ddc56f1da6d52de95857f1e2795a90f
Gous0786 <149178310+Gous0786@users.noreply.github.com>
2024-10-20 22:51:00 +0530

    Create BubbleSort.java
    Adding bubble sort in java for hacktoberfest

---
+++ BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Flag to optimize if the array is already sorted
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j + 1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped, break
+            if (!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array: " + Arrays.toString(arr));
+        
+        bubbleSort(arr);
+        
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+    }
+}
